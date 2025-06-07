eb1988d23a35b8ff8b765c43b86deb44590f71b2
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-06 21:59:50 -0500

    feat(assets): add asset type.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Asset.cs
@@ -0,0 +1,51 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+/// <summary>
+/// Represents a generic asset in the publishing system.
+/// </summary>
+/// <param name="Type">
+/// The type of the asset, which determines its specific behavior and properties.
+/// </param>
+public partial class Asset(EAssetType Type) : IPublishable
+{
+    public int Id { get; }
+
+    public AcmeAssetIdentifier AssetIdentifier { get; private set; } = new();
+    public EPublishingStatus Status { get; private set; } = EPublishingStatus.Draft;
+    public EAssetType Type { get; private set; } = Type;
+    public virtual bool Readable => false;
+    public virtual bool Viewable => false;
+    
+    
+    public void SendToEdit()
+    {
+        Status = EPublishingStatus.ReadyToEdit;
+    }
+
+    public void SendToApproval()
+    {
+        Status = EPublishingStatus.ReadyToApproval;
+    }
+
+    public void ApproveAndLock()
+    {
+        Status = EPublishingStatus.ApprovedAndLocked;
+    }
+
+    public void Reject()
+    {
+        Status = EPublishingStatus.Draft;
+    }
+
+    public void ReturnToEdit()
+    {
+        Status = EPublishingStatus.ReadyToEdit;
+    }
+
+    public virtual object GetContent()
+    {
+        return string.Empty;
+    }
+}
---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/ImageAsset.cs
@@ -0,0 +1,25 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class ImageAsset(EAssetType Type) : Asset(Type)
+{
+    public Uri? ImageUri { get;  }
+
+    public override bool Readable => false;
+    public override bool Viewable => true;
+    
+    public ImageAsset() : this(EAssetType.Image)
+    {
+    }
+
+    public ImageAsset(string imageUrl) : this(EAssetType.Image)
+    {
+        ImageUri = new Uri(imageUrl);    
+    }
+
+    public override string GetContent()
+    {
+        return ImageUri != null ? ImageUri.AbsoluteUri : string.Empty;   
+    }
+}
