c56dc3deeaf0af12a31652d374111cb01b3a5c1e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-06 22:00:23 -0500

    feat(category): add category utility.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -0,0 +1,19 @@
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+/// <summary>
+/// Represents a category for organizing content in the learning platform.
+/// </summary>
+/// <param name="name">
+/// The name of the category.
+/// </param>
+public class Category(string name)
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = name;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Category"/> class with an empty name.
+    /// </summary>
+    public Category() : this(string.Empty) {}
+    
+}
