ccf5f8afa636ba491e32c926ddf7cca3eca1ac98
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-14 12:33:09 -0500

    feat(profiles): add command and query service implementations.

---
+++ ACME.LearningCenterPlatform.API/Profiles/Application/Internal/QueryServices/ProfileQueryService.cs
@@ -0,0 +1,25 @@
+using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Queries;
+using ACME.LearningCenterPlatform.API.Profiles.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Profiles.Domain.Services;
+
+namespace ACME.LearningCenterPlatform.API.Profiles.Application.Internal.QueryServices;
+
+public class ProfileQueryService(
+    IProfileRepository profileRepository): IProfileQueryService
+{
+    public async Task<IEnumerable<Profile>> Handle(GetAllProfilesQuery query)
+    {
+        return await profileRepository.ListAsync();
+    }
+
+    public async Task<Profile?> Handle(GetProfileByIdQuery query)
+    {
+        return await profileRepository.FindByIdAsync(query.ProfileId);
+    }
+
+    public async Task<Profile?> Handle(GetProfileByEmailQuery query)
+    {
+        return await profileRepository.FindByEmailAsync(query.Email);
+    }
+}
