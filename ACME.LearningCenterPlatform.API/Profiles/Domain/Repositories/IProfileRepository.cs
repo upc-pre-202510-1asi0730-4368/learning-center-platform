b35da8a80ce3343cc54d60d95966cf965967f7ce
Angel Velasquez <pcisavel@upc.edu.pe>
2025-06-14 11:56:14 -0500

    feat(profiles): add outbound service contract.

---
+++ ACME.LearningCenterPlatform.API/Profiles/Domain/Repositories/IProfileRepository.cs
@@ -0,0 +1,9 @@
+using ACME.LearningCenterPlatform.API.Profiles.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.Profiles.Domain.Repositories;
+
+public interface IProfileRepository : IBaseRepository<Profile>
+{
+    Task<Profile?> FindByEmailAsync(string email);
+}
